48193c7facc6974f3175e986e1141c3f707ced82
Mary Mazi <mazi.mary.o@gmail.com>
2019-07-13 06:33:05 +0100

    add bootstrap

--- src/index.js
+++ src/index.js
@@ -3,6 +3,7 @@ import ReactDOM from 'react-dom';
 import { BrowserRouter as Router } from 'react-router-dom';
 import { Provider } from 'react-redux';
 import configureStore from 'store/configureStore';
+import 'bootstrap/dist/css/bootstrap.css';
 import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
--- src/views/login/index.js
+++ src/views/login/index.js
@@ -1,7 +1,7 @@
 import React from 'react';
 
 const Login = ({ title }) => (
-  <h3>{title} Page</h3>
+  <h3 className="border border-primary">{title} Page</h3>
 );
 
 export default Login;
