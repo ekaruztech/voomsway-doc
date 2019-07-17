cd9b0ac1dba07778bf19357f8923fe7c09cb936c
Mary Mazi <mazi.mary.o@gmail.com>
2019-07-17 17:12:50 +0100

    update loginHook to return login data from store

--- src/views/login/index.js
+++ src/views/login/index.js
@@ -1,12 +1,13 @@
 import React, { useState } from 'react';
+import { withRouter } from 'react-router-dom';
 import { Form, Button } from 'react-bootstrap';
 import { useLogin } from 'views/login/loginHooks';
 
 
-const LoginForm = ({ title }) => {
+const LoginForm = ({ title, history }) => {
   const [email, setEmail] = useState('');
   const [password, setPassword] = useState('');
-  const { loginUser } = useLogin();
+  const { loginUser, loginData } = useLogin();
 
   const handleEmailChange = event => (
     setEmail(event.target.value)
@@ -18,16 +19,26 @@ const LoginForm = ({ title }) => {
 
   const handleLoginSubmit = () => {
     const userData  = { email, password };
-    loginUser(userData).then(res => console.log(res))
+    loginUser(userData, history);
   }
 
   return (
     <div className="login-container flex-center">
       <Form className="login-form">
         <h4 className="text-center padding-bottom24">{title}</h4>
+        {
+          loginData.error.details && 
+          Object.keys(loginData.error.details).length === 0 
+          && loginData.error.key &&
+          <span className="help-block">{loginData.error.key}</span>
+        }
 
         <Form.Group>
           <Form.Label>Email Address</Form.Label>
+          {
+            loginData.error.details && loginData.error.details.email && 
+            <span className="help-block">{loginData.error.details.email}</span>
+          }
           <Form.Control 
             type="email" 
             placeholder="name@example.com"
@@ -38,6 +49,10 @@ const LoginForm = ({ title }) => {
 
         <Form.Group>
           <Form.Label>Password</Form.Label>
+          {
+            loginData.error.details && loginData.error.details.password && 
+            <span className="help-block">{loginData.error.details.password}</span>
+          }
           <Form.Control
             type="password"
             placeholder="Enter your password"
@@ -58,4 +73,4 @@ const LoginForm = ({ title }) => {
   );
 }
 
-export default LoginForm;
+export default withRouter(LoginForm);
--- src/views/login/loginHooks.js
+++ src/views/login/loginHooks.js
@@ -1,13 +1,15 @@
 import { useCallback } from 'react';
-import { useDispatch } from 'react-redux';
+import { useDispatch, useSelector, } from 'react-redux';
 import { userLoginRequest } from 'store/actions/login';
 
 export const useLogin = () => {
   const dispatch = useDispatch();
   const loginUser = useCallback(
-    userData => dispatch(userLoginRequest(userData)),
+    (userData,history) => dispatch(userLoginRequest(userData, history)),
     [dispatch]
   );
 
-  return { loginUser };
+  const loginData = useSelector(state => state.login );
+
+  return { loginUser, loginData };
 };
