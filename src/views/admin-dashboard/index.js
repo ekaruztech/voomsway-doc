d236fca20c9adff2ddd2a985c8d73a8fed3d8e4b
Mary Mazi <mazi.mary.o@gmail.com>
2019-07-23 18:04:40 +0100

    - add dashboard that should list available modules in the db - create form for adding documentation

--- src/views/admin-dashboard/index.js
+++ src/views/admin-dashboard/index.js
@@ -1,16 +1,70 @@
 import React from 'react';
-import { withRouter } from 'react-router-dom';
+import { withRouter, Link } from 'react-router-dom';
 import  {isLoggedIn } from 'services/isLoggedIn';
+import { Table, Badge, Button } from 'react-bootstrap';
+
+
+const DocList = () => (
+  <Table striped bordered hover responsive>
+    <thead>
+      <tr>
+        <th>#</th>
+        <th>Module</th>
+        <th>Last Edited</th>
+        <th>View | Edit</th>
+      </tr>
+    </thead>
+    <tbody>
+      <tr>
+        <td>1</td>
+        <td>Authentication</td>
+        <td><Badge variant="dark">07/07/2019</Badge> - elshady@gmail.com</td>
+        <td>
+          <Link to="#">View    </Link>
+          <Link to="#">Edit</Link>
+        </td>
+      </tr>
+      <tr>
+        <td>2</td>
+        <td>Account</td>
+        <td><Badge variant="dark">07/07/2019</Badge> - mary@gmail.com</td>
+        <td>
+          <Link to="#">View    </Link>
+          <Link to="#">Edit</Link>
+        </td>
+      </tr>
+      <tr>
+        <td>3</td>
+        <td>Staff</td>
+        <td><Badge variant="dark">07/07/2019</Badge> - mary@gmail.com</td>
+        <td>
+          <Link to="#">View    </Link>
+          <Link to="#">Edit</Link>
+        </td>
+      </tr>
+    </tbody>
+  </Table>
+);
+
 
 const AdminDashboard = ({ title, requirePermission, history }) => {
   const isAuthorized = requirePermission && isLoggedIn();
   
   return (
-    <>
-      <h3>{title} - Dashboard</h3>
-      
+    <>  
       { isAuthorized ?
-          <h3>Admin here</h3>
+          <>       
+            <h3 className="inline-block">{title} Dashboard</h3>
+
+            <div className="float-right add-module-btn">
+              <Button variant="info" size="sm"
+                onClick={() => history.push('/admin/new-module')}
+              >
+                Add Module
+              </Button>
+            </div>
+            <DocList />
+          </>
         :
           history.push('/admin/login')}
       }
